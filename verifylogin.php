41fae2683ca1ba467b5cf548229fde8164bacfaf
prnkmr <praveen.k9894@gmail.com>
2015-03-12 12:16:10 +0530

    edit verifylogin

--- verifylogin.php
+++ verifylogin.php
@@ -1,3 +1,13 @@
 <?php
+require_once("praveenlib.php");
+$keys=array('uname','password');
+if(checkPOST($keys)){
+    $connection=connectSQL($dbdetails);
 
+    $uname=safeString($connection,$_POST['uname']);
+    $password=safeString($connection,$_POST['password']);
+    
+}else{
+    echo "insufficient credentials";
+}
 
