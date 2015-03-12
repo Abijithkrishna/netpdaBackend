6dbb3d76392878a65d15bb84511105ac3246cf7b
AbijithKrishna <abijitharc@gmail.com>
2015-03-12 11:40:41 +0530

    Praveen libray and datas.php + empty signup page

---
+++ datas.php
@@ -0,0 +1,13 @@
+<?php
+$dbURL="localhost";
+$dbName="student";
+$dbusername="root";
+$dbpassword="database";
+$dbdetails=array(
+    'url'=>$dbURL,
+    'name'=>$dbName,
+    'username'=>$dbusername,
+    'password'=>$dbpassword
+);
+
+?>
